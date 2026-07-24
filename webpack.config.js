44c1a02d3cee9b7be1254dbb3d58a0c53b041fae
gdejules <gd.juliarthaa@gmail.com>
2026-07-24 17:54:44 +0800

    chore: add webpack-dev-server and configure devServer options in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ export default {
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
   },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/app.html"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/app.html",
