052926ee98d4b6453a3169a3030ea91f6cfe650b
gdejules <gd.juliarthaa@gmail.com>
2026-07-25 18:06:05 +0800

    Set up font and spacing styling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import "./styles.css";
 
 const mainContent = document.getElementById("content");
 
-const greeting = document.createElement("span");
+const greeting = document.createElement("p");
 greeting.textContent = "Let's create your todo list!";
 mainContent.appendChild(greeting);
